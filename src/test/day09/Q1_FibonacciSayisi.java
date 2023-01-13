881e2a4ac66084ef1fec25a6526f2aa98aaa7b43
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-01-13 13:27:09 +0300

    13.01.23

--- src/test/day09/Q1_FibonacciSayisi.java
+++ src/test/day09/Q1_FibonacciSayisi.java
@@ -1,2 +1,17 @@
-package test.day09;public class Q1_FibonacciSayisi {
+package test.day09;
+
+public class Q1_FibonacciSayisi {
+    public static void main(String[] args) {
+        /*
+         * Write a method which accepts an integer as parameter
+         * and returns “true” if it is a Fibonacci number
+         * Then check the result and print
+         * “Fibonacci series contain your number” or
+         * “Fibonacci series contain that numbers : X and Y”
+         * (X and Y should be the closest Fibonacci numbers to given number X<num<Y)
+         */
+
+
+    }
+
 }
